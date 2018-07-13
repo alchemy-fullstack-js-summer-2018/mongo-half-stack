6c13fcf27fbcc17e3e2420d674e98aa3ea11da49
MariahAdams <mariah.adams23@yahoo.com>
2018-07-13 10:01:16 -0700

    mongo.js file setup

---
+++ mongo.js
@@ -0,0 +1,22 @@
+
+const { MongoClient/* , ObjectId */ } = require('mongodb');
+
+const url = 'mongodb://localhost:27017/treats';
+let client = null;
+MongoClient.connect(url, { useNewUrlParser: true })
+    .then(_client => {
+        client = _client;
+        const db = client.db();
+        return db.collection('pies');
+    })
+    .then(pies => {
+        console.log(
+            JSON.stringify(pies, true, 1)
+        );
+    })
+    .catch(err => {
+        console.log('FAIL!', err);
+    })
+    .then(() => {
+        client.close();
+    });
