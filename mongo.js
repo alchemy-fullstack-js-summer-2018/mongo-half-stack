33f8e9b37d1b266ba419dce3a0a8ae69deed68d9
MariahAdams <mariah.adams23@yahoo.com>
2018-07-13 10:27:52 -0700

    working on getting mongodb up and running

--- mongo.js
+++ mongo.js
@@ -1,5 +1,10 @@
 
+
+
+
+
 const { MongoClient/* , ObjectId */ } = require('mongodb');
+
 
 const url = 'mongodb://localhost:27017/treats';
 let client = null;
@@ -7,7 +12,12 @@ MongoClient.connect(url, { useNewUrlParser: true })
     .then(_client => {
         client = _client;
         const db = client.db();
-        return db.collection('pies');
+        return db.collection('pies')
+            .find()
+            .toArray()
+
+            .insert({ name: 'creampie' })
+            .then(result => result.ops[0]);
     })
     .then(pies => {
         console.log(
