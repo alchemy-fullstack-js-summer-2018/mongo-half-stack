cfd4472e410ef10efdde02b1b57b5980997ca7a6
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-12 17:44:25 -0700

    Add test for players route

---
+++ test/players.test.js
@@ -0,0 +1,38 @@
+require('dotenv').config({ path: './test/.env' });
+const mongo = require('../lib/mongodb');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../lib/app');
+
+chai.use(chaiHttp);
+const { assert } = chai;
+
+describe('Players API', () => {
+    beforeEach(() => {
+        return mongo.then(db => {
+            db.collection('players').remove();
+        });
+    });
+
+    let player = {};
+    
+    beforeEach(() => {
+        const data = {
+            name: 'Injoong',
+            kills: 0,
+            wins: 1
+        };
+        return chai.request(app)
+            .post('/players')
+            .send(data)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, data.name);
+                player = body;
+            });
+    });
+
+    it('gets players', () => {
+
+    });
+});
