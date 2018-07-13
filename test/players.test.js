6532f8541ef096d468765a390f37be9dcf3640af
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-13 09:54:26 -0700

    Implement get functionality and test

--- test/players.test.js
+++ test/players.test.js
@@ -5,29 +5,43 @@ const request = require('./request');
 describe('Players API', () => {
     beforeEach(() => {
         return mongo.then(db => {
-            db.collection('players').remove();
+            db.collection('leaderboards').remove();
         });
     });
 
-    let player = {};
+    let savedPlayers = [];
+
+    const save = player => {
+        return request
+            .post('/players')
+            .send(player)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, player.name);
+                savedPlayers.push(body);
+            });
+    };
     
     beforeEach(() => {
-        const data = {
+        return save({
             name: 'Injoong',
             kills: 0,
             wins: 1
-        };
-        return request
-            .post('/players')
-            .send(data)
-            .then(({ body }) => {
-                assert.ok(body._id);
-                assert.equal(body.name, data.name);
-                player = body;
-            });
+        });
+    });
+    beforeEach(() => {
+        return save({
+            name: 'Arthur',
+            kills: 10,
+            wins: 2
+        });
     });
 
     it('gets players', () => {
-        console.log(player);
+        return request
+            .get('/players')
+            .then(({ body }) => {
+                assert.deepEqual(body, savedPlayers);
+            });
     });
 });
