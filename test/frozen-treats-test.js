4c19a21528251267046dee42a5f5200923bd870e
Mario Quintana <pyrros2000@gmail.com>
2018-07-13 17:19:33 -0700

    saving soda test and frozen treat now passing

--- test/frozen-treats-test.js
+++ test/frozen-treats-test.js
@@ -10,6 +10,27 @@ describe('frozen treats API', () => {
         });
     });
 
+    let frozenTreat; 
+    beforeEach(() => {
+        const data = {
+            name: 'ice cream',
+            flavor: 'vanilla'
+        };
+
+        return request
+            .post('/frozenTreats')
+            .send(data)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, data.name);
+                frozenTreat = body;
+            });
+    });
+
+    it('saves a frozen treat', () => {
+        assert.ok(frozenTreat._id);
+    });
+
     it('returns 404 or bad url', () => {
         return request  
             .get('/bad')
