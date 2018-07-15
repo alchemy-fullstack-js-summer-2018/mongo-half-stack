9814294246df8779b1618e7e63f0e68e7ce783e8
MariahAdams <mariah.adams23@yahoo.com>
2018-07-15 16:11:38 -0700

    working on first test for cakes, need to fix bug in code

---
+++ test/cakes.test.js
@@ -0,0 +1,40 @@
+const mongo = require('../lib/mongodb');
+const { assert } = require('chai');
+const request = require('./request');
+
+describe('Cakes API', () => {
+
+    beforeEach(() => {
+        return mongo.then(db => {
+            return db.collection('cakes').remove();
+        });
+    });
+
+    let cake;
+    beforeEach(() => {
+        const data = {
+            name: 'Lamington'
+        };
+
+        return request 
+            .post('/cakes')
+            .send(data)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, data.name);
+                cake = body;
+            });
+    });
+
+    it('saves a cake', () => {
+        assert.ok(cake._id);
+    });
+
+    it('returns 404 bad url', () => {
+        return request
+            .get('/bad') 
+            .then(res => {
+                assert.equal(res.status, 404);
+            });
+    });
+});
