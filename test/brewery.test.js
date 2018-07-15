c6bad393d4bad5ac17d2dbec3d0f34056d420cc1
Robyn Navarro <rnavarro182@gmail.com>
2018-07-14 17:31:15 -0700

    added a second beer

--- test/brewery.test.js
+++ test/brewery.test.js
@@ -3,8 +3,6 @@ const { assert } = require('chai');
 const request = require('./request');
 
 describe('Brewery API', () => {
-
-    let beers = [];
     
     beforeEach(() => {
         return mongo.then(db => {
@@ -12,19 +10,11 @@ describe('Brewery API', () => {
         });
     });
     
-    let maibock = {
-        brewery: 'Rogue Brewery',
-        name: 'Dead Guy Ale',
-        origin: 'Portland, OR'
-    };
-    let stout = {
-        brewery: 'Guinness',
-        name: 'Extra Stout',
-        origin: 'Dublin, Ireland'
-    };
-    let beer;
+    let maibock;
+    let stout;
+
     beforeEach(() => {
-        const data = {
+        const beer1 = {
             brewery: 'Rogue Brewery',
             name: 'Dead Guy Ale',
             origin: 'Portland, OR'
@@ -32,16 +22,32 @@ describe('Brewery API', () => {
 
         return request
             .post('/brewery')
-            .send(data)
+            .send(beer1)
             .then(({ body }) => {
                 assert.ok(body._id);
-                assert.equal(body.name, data.name);
-                beer = body;
+                assert.equal(body.name, beer1.name);
+                maibock = body;
+            });
+    });
+    beforeEach(() => {
+        const beer2 = {
+            brewery: 'Guinness',
+            name: 'Extra Stout',
+            origin: 'Dublin, Ireland'
+        };
+
+        return request
+            .post('/brewery')
+            .send(beer2)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, beer2.name);
+                stout = body;
             });
     });
 
     it('Saves a beer', () => {
-        assert.ok(beer._id);
+        assert.ok(maibock._id);
     });
 
     it('Returns 404 on bad url', () => {
@@ -54,17 +60,17 @@ describe('Brewery API', () => {
 
     it('Orders a beer by id', () => {
         return request
-            .get(`/brewery/${beer._id}`)
+            .get(`/brewery/${maibock._id}`)
             .then(({ body }) => {
-                assert.equal(body._id, beer._id);
+                assert.equal(body._id, maibock._id);
             });
     });
 
     it('Updates beer', () => {
-        beer.origin = 'Portland';
+        maibock.origin = 'Portland';
         return request
-            .put(`/brewery/${beer._id}`)
-            .send(beer)
+            .put(`/brewery/${maibock._id}`)
+            .send(maibock)
             .then(({ body }) => {
                 assert.equal(body.origin, 'Portland');
             });
@@ -72,7 +78,7 @@ describe('Brewery API', () => {
 
     it('Drinks beer', () => {
         return request
-            .del(`/brewery/${beer._id}`)
+            .del(`/brewery/${maibock._id}`)
             .then(({ body }) => {
                 assert.equal(body.removed, true);
             });
@@ -82,7 +88,7 @@ describe('Brewery API', () => {
         return request
             .get('/brewery')
             .then(({ body }) => {
-                assert.deepEqual(body, [beer]);
+                assert.deepEqual(body, [maibock, stout]);
             });
 
     });
