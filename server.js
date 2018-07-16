463c943826fe22408ce399edaa31f0efa5beca7c
Mario Quintana <pyrros2000@gmail.com>
2018-07-16 11:01:35 -0700

    npm i mongodb

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 3000;
 const server  = createServer(app);
 
 server.listen(PORT, () => {
-    console.log('server running on', server.address().port);
+    console.log('server running on', PORT);
 });
