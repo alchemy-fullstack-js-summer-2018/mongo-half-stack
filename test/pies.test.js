bf6dd447a41bc5eb02c201e60e85f6fb95165c75
MariahAdams <mariah.adams23@yahoo.com>
2018-07-16 09:14:00 -0700

    fixed unhandled promise rejection in notFound file

--- test/pies.test.js
+++ test/pies.test.js
@@ -1,77 +1,78 @@
-// const mongo = require('../lib/mongodb');
-// const { assert } = require('chai');
-// const request = require('./request');
+const mongo = require('../lib/mongodb');
+const { assert } = require('chai');
+const request = require('./request');
 
-// describe.only('pies api', () => {
+describe.only('pies api', () => {
 
-//     beforeEach(() => {
-//         return mongo.then(db => {
-//             return db.collection('pies').remove();
-//         });
-//     });
+    beforeEach(() => {
+        return mongo.then(db => {
+            return db.collection('pies').remove();
+        });
+    });
 
-//     let pie;
-//     beforeEach(() => {
-//         const data = {
-//             name: 'creampie'
-//         };
+    let pie;
+    beforeEach(() => {
+        const data = {
+            name: 'creampie'
+        };
 
-//         return request
-//             .post('/pies')
-//             .send(data)
-//             .then(({ body }) => {
-//                 assert.ok(body._id);
-//                 assert.equal(body.name, 'creampie');
-//                 pie = body;
-//             });
-//     });
+        return request
+            .post('/pies')
+            .send(data)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.name, 'creampie');
+                pie = body;
+            });
+    });
 
-//     it('saves a pie', () => {
-//         assert.ok(pie._id);
-//     });
+    
+    it('saves a pie', () => {
+        assert.ok(pie._id);
+    });
 
-//     it('returns 404 bad url', () => {
-//         return request
-//             .get('/bad') 
-//             .then(res => {
-//                 assert.equal(res.status, 404);
-//             });
-//     });
+    it('gets pies', () => {
+        return request
+            .get('/pies')
+            .then(({ body }) => {
+                assert.deepEqual(body, [pie]);
+            });
+    });
 
-//     it('gets pies', () => {
-//         return request
-//             .get('/pies')
-//             .then(({ body }) => {
-//                 assert.deepEqual(body, [pie]);
-//             });
-//     });
+    it('get a pie by id', () => {
+        return request
+            .get(`/pies/${pie._id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body, pie);
+            });
+    });
 
-//     it('get a pie by id', () => {
-//         return request
-//             .get(`/pies/${pie._id}`)
-//             .then(({ body }) => {
-//                 assert.deepEqual(body, pie);
-//             });
-//     });
+    it('updates a pie', () => {
+        pie.name = 'cherry pie';
+        return request
+            .put(`/pies/${pie._id}`)
+            .send(pie)
+            .then(({ body }) => {
+                assert.deepEqual(body, pie);
+            });
+    });
 
-//     it('updates a pie', () => {
-//         pie.name = 'cherry pie';
-//         return request
-//             .put(`/pies/${pie._id}`)
-//             .send(pie)
-//             .then(({ body }) => {
-//                 assert.deepEqual(body, pie);
-//             });
-//     });
+    it('removes a pie', () => {
+        return request
+            .del(`/pies/${pie._id}`)
+            .then(() => {
+                return request.get('/pies');
+            })
+            .then(({ body }) => {
+                assert.deepEqual(body, []);
+            });
+    });
 
-//     it('removes a pie', () => {
-//         return request
-//             .del(`/pies/${pie._id}`)
-//             .then(() => {
-//                 return request.get('/pies');
-//             })
-//             .then(({ body }) => {
-//                 assert.deepEqual(body, []);
-//             });
-//     });
-// });
+    it('returns 404 bad url', () => {
+        return request
+            .get('/bad') 
+            .then(res => {
+                assert.equal(res.status, 404);
+            });
+    });
+});
