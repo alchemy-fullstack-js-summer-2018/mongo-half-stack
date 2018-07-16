35577597e29a22565636c0b631595f3fa3ec119a
Robyn Navarro <rnavarro182@gmail.com>
2018-07-16 10:03:07 -0700

    fixed malformed URI error

--- test/db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: './test/.env.test' || 'mongodb://localhost:27017/carbyn' });
-const mongo = require('../lib/mongodb');
-
-after(() => {
-    mongo.client.close();
-});
